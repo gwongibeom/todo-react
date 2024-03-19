645d49ba1acf4dcc859cea1b1d7752021e726626
gwongibeom <k201gun@kakao.com>
2024-03-19 10:00:44 +0900

    todo 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
 
-const mookData = [
+const mockData = [
   {
     id: 0,
     isDone: false,
@@ -26,13 +26,25 @@ const mookData = [
 ]
 
 function App() {
-  const [todos, setTodos] = useState(mookData)
+  const [todos, setTodos] = useState(mockData)
+  const idRef = useRef(3)
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    }
+
+    setTodos([newTodo, ...todos])
+  }
 
   return (
     <div className='App'>
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
   )
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,40 @@
 import './Editor.css'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState('')
+  const contentRef = useRef()
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value)
+  }
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit()
+    }
+  }
+
+  const onSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus()
+      console.log('거부')
+      return
+    }
+    onCreate(content)
+    setContent('')
+  }
+
   return (
     <div className='Editor'>
-      <input placeholder='새로운 Todo...' />
-      <button>추가</button>
+      <input
+        placeholder='새로운 Todo...'
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        value={content}
+        ref={contentRef}
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   )
 }
